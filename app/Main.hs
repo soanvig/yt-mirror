e596da76c13e050f503aef7652bc66151c70b5e6
Mateusz Koteja <mateusz.koteja@leocode.com>
2022-01-16 13:09:46 +0100

    feat: add Dependencies module

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,22 @@ module Main where
 import Lib
 import System.IO
 import Program
+import Dependencies
+import System.Exit (die)
+import Control.Monad (void)
+import GHC.OldList (intercalate)
 
 main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
 
+  dependencyResult <- verifyDependencies
+
   command <- getCommand
+
+  void $ case dependencyResult of
+    Right _ -> return ()
+    Left executables -> die $ "Missing dependencies in PATH: " ++ intercalate "," executables
 
   case command of
     Prepare prepareOptions -> prepare prepareOptions
