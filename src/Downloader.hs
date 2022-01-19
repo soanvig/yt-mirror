9d7e5511f2e62518dc41ec00018f943a308ed1d6
Mateusz Koteja <mateusz.koteja@leocode.com>
2022-01-19 18:10:26 +0100

    feat: add "skipped" state

--- src/Downloader.hs
+++ src/Downloader.hs
@@ -16,44 +16,48 @@ import qualified Logger as L
 import Data.String.Interpolate
 import Helpers
 
-getDownloadParams :: String -> FilePath -> FilePath -> [String]
-getDownloadParams youtubeId tmpDir targetDir = [
+getDownloadParams :: String -> FilePath -> FilePath -> String -> [String]
+getDownloadParams youtubeId tmpDir targetDir matchFilter = [
   "-x"
   , "-o"
   , [iii|#{tmpDir}/%(title)s.%(ext)s|]
   , "-q"
   , "--no-warnings"
   , "--exec"
-  , [iii|mv {} #{targetDir}/|]
+  , [iii|mv {} #{targetDir}/"|]
+  , "--match-filter"
+  , if matchFilter == "" then "comment_count >? 0" else matchFilter
   , "--"
   , youtubeId
   ]
 
 data DownloadSaverMsg = SynchronizationFinished String
   | DownloadFailed String String
+  | DownloadSkipped String
   deriving (Show)
 
 -- public
 
 newtype DownloaderMsg = DownloaderStart Process deriving (Show)
 
-downloader :: A.ActorRef DownloadSaverMsg -> FilePath -> FilePath -> A.ActorId -> A.Behavior DownloaderMsg
-downloader saverActor tmpDir targetDir currentActorId = A.Behavior $ \case
+downloader :: A.ActorRef DownloadSaverMsg -> FilePath -> FilePath -> String -> A.ActorId -> A.Behavior DownloaderMsg
+downloader saverActor tmpDir targetDir matchFilter currentActorId = A.Behavior $ \case
   DownloaderStart process -> do
     let youtubeId = processYoutubeId process
                     
     L.log (L.SynchronizationStarted youtubeId currentActorId)
 
-    let shellProcess = (proc "yt-dlp" (getDownloadParams youtubeId tmpDir targetDir)) {
+    let shellProcess = (proc "yt-dlp" (getDownloadParams youtubeId tmpDir targetDir matchFilter)) {
       std_in  = UseHandle stdin,
       std_out = CreatePipe,
       std_err = CreatePipe
     }
 
-    processHandler@(_, _, Just hErr, p) <- createProcess_ "" shellProcess
+    processHandler@(_, Just hOut, Just hErr, p) <- createProcess_ "" shellProcess
 
     exitCode <- waitForProcess p
     errorMessage <- hGetContents hErr
+    outputMessage <- hGetContents hOut
     
     -- What is the consequence of not closing the handles?
     -- If they are closed with `withCreateProcess` or with `cleanupProcess`
@@ -61,7 +65,9 @@ downloader saverActor tmpDir targetDir currentActorId = A.Behavior $ \case
     -- According to this: https://stackoverflow.com/questions/32337336/do-i-need-to-call-closehandle
     -- quitting process should automatically close handles by the system
     -- cleanupProcess processHandler
-    
+
+    -- @TODO detect and save "skipped" state
+
     case exitCode of
       ExitFailure _ -> do
         A.send saverActor (DownloadFailed youtubeId errorMessage)
@@ -70,7 +76,7 @@ downloader saverActor tmpDir targetDir currentActorId = A.Behavior $ \case
         A.send saverActor (SynchronizationFinished youtubeId)
         L.log (L.SynchronizationFinished youtubeId currentActorId)
     
-    return (downloader saverActor tmpDir targetDir currentActorId)
+    return (downloader saverActor tmpDir targetDir matchFilter currentActorId)
 
 downloadSaver :: FilePath -> TVar Int -> A.Behavior DownloadSaverMsg
 downloadSaver processPath processedCounter = A.Behavior $ \case
@@ -81,6 +87,10 @@ downloadSaver processPath processedCounter = A.Behavior $ \case
   DownloadFailed youtubeId errorMessage -> do
     let pureErrorMessage = trim . replace '\n' ' ' $ errorMessage
     PR.openRepository processPath (PR.errorProcess youtubeId pureErrorMessage)
+    atomically $ modifyTVar processedCounter (+ 1)
+    return (downloadSaver processPath processedCounter)
+  DownloadSkipped youtubeId -> do
+    PR.openRepository processPath (PR.skipProcess youtubeId)
     atomically $ modifyTVar processedCounter (+ 1)
     return (downloadSaver processPath processedCounter)
 
